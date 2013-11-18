17d86d4664eab170dcfa08399f23da796ca62d75
Soumya Batra <soumyab@andrew.cmu.edu>
2013-11-18 15:24:30 -0500

    Added Relative path for stopwords.txt

--- hw4-soumyab/src/main/java/edu/cmu/lti/f13/hw4/hw4_soumyab/annotators/DocumentVectorAnnotator.java
+++ hw4-soumyab/src/main/java/edu/cmu/lti/f13/hw4/hw4_soumyab/annotators/DocumentVectorAnnotator.java
@@ -1,9 +1,11 @@
 package edu.cmu.lti.f13.hw4.hw4_soumyab.annotators;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
@@ -22,96 +24,104 @@ import edu.cmu.lti.f13.hw4.hw4_soumyab.utils.Utils;
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
-  @Override
-  public void process(JCas jcas) throws AnalysisEngineProcessException {
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-    FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
-    if (iter.isValid()) {
-      iter.moveToNext();
-      Document doc = (Document) iter.get();
-      try {
-        createTermFreqVector(jcas, doc);
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
+		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
+		if (iter.isValid()) {
+			iter.moveToNext();
+			Document doc = (Document) iter.get();
+			try {
+				createTermFreqVector(jcas, doc);
+			} catch (IOException e) {
+				// Print stack trace
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// Print stack trace
+				e.printStackTrace();
+			}
+		}
 
-  }
+	}
 
-  /**
-   * Constructs a vector of tokens and update the tokenList in CAS
-   * 
-   * @param jcas
-   *          - JCAS object
-   * @param doc
-   *          - Document object
-   * @throws IOException
-   */
+	/**
+	 * Constructs a vector of tokens and update the tokenList in CAS
+	 * 
+	 * @param jcas
+	 *            - JCAS object
+	 * @param doc
+	 *            - Document object
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
 
-  private void createTermFreqVector(JCas jcas, Document doc) throws IOException {
+	private void createTermFreqVector(JCas jcas, Document doc)
+			throws IOException, URISyntaxException {
 
-    // Get text of the document
-    String docText = doc.getText();
+		// Get text of the document
+		String docText = doc.getText();
 
-    // Get stop words
-    ArrayList<String> stopWords = new ArrayList<String>();
-    stopWords = getStopWords();
+		// Get stop words
+		ArrayList<String> stopWords = new ArrayList<String>();
+		stopWords = getStopWords();
 
-    // Create temporary structures to store tokens
-    ArrayList<Token> toklist = new ArrayList<Token>();
-    HashMap<String, Integer> tlist = new HashMap<String, Integer>();
+		// Create temporary structures to store tokens
+		ArrayList<Token> toklist = new ArrayList<Token>();
+		HashMap<String, Integer> tlist = new HashMap<String, Integer>();
 
-    // Tokenizing based on possible word separators
-    StringTokenizer st = new StringTokenizer(docText, " .,!?");
+		// Tokenizing based on possible word separators
+		StringTokenizer st = new StringTokenizer(docText, " .,!?");
 
-    // Putting token strings corresponding to their frequencies in the Hashmap
-    Integer i = 0;
-    while (st.hasMoreTokens()) {
-      String token = st.nextToken();
+		// Putting token strings corresponding to their frequencies in the
+		// Hashmap
+		Integer i = 0;
+		while (st.hasMoreTokens()) {
+			String token = st.nextToken();
 
-      if (!(stopWords.contains(token))) {
-        if ((i = tlist.get(token)) != null)
-          tlist.put(token.toLowerCase(), i + 1);
-        else
-          tlist.put(token.toLowerCase(), 1);
-     }
-    }
+			if (!(stopWords.contains(token))) {
+				if ((i = tlist.get(token)) != null)
+					tlist.put(token.toLowerCase(), i + 1);
+				else
+					tlist.put(token.toLowerCase(), 1);
+			}
+		}
 
-    // Converting from Hashmap to an ArrayList of tokens
-    for (String t : tlist.keySet()) {
-      Token tok = new Token(jcas);
-      tok.setText(t);
-      tok.setFrequency(tlist.get(t));
-      toklist.add(tok);
-    }
+		// Converting from Hashmap to an ArrayList of tokens
+		for (String t : tlist.keySet()) {
+			Token tok = new Token(jcas);
+			tok.setText(t);
+			tok.setFrequency(tlist.get(t));
+			toklist.add(tok);
+		}
 
-    // Finally, setting the token list of a document after converting array list to FS List
-    doc.setTokenList(Utils.fromCollectionToFSList(jcas, toklist));
+		// Finally, setting the token list of a document after converting array
+		// list to FS List
+		doc.setTokenList(Utils.fromCollectionToFSList(jcas, toklist));
 
-  }
+	}
 
-  private ArrayList<String> getStopWords() throws IOException {
-    ArrayList<String> list = new ArrayList<String>();
-    BufferedReader br = new BufferedReader(new FileReader(
-            "C:/eclipse/Soumya/workspace/hw4-soumyab/src/main/resources/stopwords.txt"));
+	private ArrayList<String> getStopWords() throws IOException,
+			URISyntaxException {
+		ArrayList<String> list = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(new File(
+				getClass().getResource("/stopwords.txt").toURI())));
 
-    String line = br.readLine();
-    try {
-      for (int i = 0; i < 2; i++)
-        line = br.readLine();
+		String line = br.readLine();
+		try {
+			for (int i = 0; i < 2; i++)
+				line = br.readLine();
 
-      while (line != null) {
-        list.add(line);
-        line = br.readLine();
-      }
+			while (line != null) {
+				list.add(line);
+				line = br.readLine();
+			}
 
-    } finally {
-      if (br != null)
-        br.close();
-    }
+		} finally {
+			if (br != null)
+				br.close();
+		}
 
-    return list;
-  }
+		return list;
+	}
 
 }
